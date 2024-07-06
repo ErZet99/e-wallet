c09efc01d335ec8a0c2e7c5cbc31e95aa9085db6
ErZet99 <patryk231123@gmail.com>
2024-07-06 20:00:13 +0200

    Init commit

---
+++ backend/src/test/java/com/github/ErZet99/EWalletApp/EWalletAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ErZet99.EWalletApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EWalletAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
